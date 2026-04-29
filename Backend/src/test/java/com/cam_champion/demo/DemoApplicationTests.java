495c10b4b33034c4bf9baabf8e67e6cb3c67802f
Ashley Nkuna <ashleynkuna33@gmail.com>
2026-04-30 01:57:38 +0200

    Adding spring initializer files

---
+++ Backend/src/test/java/com/cam_champion/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cam_champion.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
