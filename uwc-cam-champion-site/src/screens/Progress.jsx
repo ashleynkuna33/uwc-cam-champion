e68d26b347aee6624dba66d4263f61eadbae4b34
Tshepho Mamoraro Maimela <4323138@myuwc.ac.za>
2026-06-01 00:55:41 +0200

    I have created the second section of the ,but incomplete

--- uwc-cam-champion-site/src/screens/Progress.jsx
+++ uwc-cam-champion-site/src/screens/Progress.jsx
@@ -33,7 +33,96 @@ function Progress() {
 
        </div>
 
-      </div>
+
+    <div className="global-container">
+
+     <div className="left">
+
+        <div className="progress-bar-container">
+
+            <div className="progress-bar-heading">
+                <p>CURRENT CAM</p>
+                <span>63%</span>
+            </div>
+            <div className="bar-line" data-percent="90%">
+                <span style={{width:"90%"}}  className="bar-fill"></span>
+            </div>
+        </div>
+        
+    
+
+       
+
+        <div className="progress-bar-container">
+
+            <div className="progress-bar-heading">
+                <p>PROJECTED FINAL</p>
+                <span>79%</span>
+            </div>
+            <div className="bar-line" data-percent="79%">
+                <span style={{width:"79%"}}  className="bar-fill"></span>
+            </div>
+
+
+        </div>
+
+        </div>
+        
+
+        
+     <div className="right">
+        <div className="progress-bar-container">
+
+            <div className="progress-bar-heading">
+                <p>REMAINING WEIGHT</p>
+                <span>30%</span>
+            </div>
+            <div className="bar-line" data-percent="30%">
+                <span style={{width:"30%"}}  className="bar-fill"></span>
+            </div>
+
+
+        </div>
+
+
+        <div className="progress-bar-container">
+
+            <div className="progress-bar-heading">
+                <p>NEED FOR DINSTINCTION</p>
+                <span>80%</span>
+            </div>
+            <div className="bar-line" data-percent="80%">
+                <span style={{width:"80%"}}  className="bar-fill"></span>
+            </div>
+
+
+        </div>
+
+        </div>
+
+    </div>
+
+
+
+
+        <div className="cards-container">
+
+            <div className="left-card">
+
+            </div>
+
+
+
+            
+
+            <div className="right-card">
+
+
+            </div>
+        </div>
+        
+
+    </div>
         </>   
     );
 }
