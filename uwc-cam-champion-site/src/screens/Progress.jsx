725c2cd71338a24908cc2d6b56b0cfaff201093f
Tshepho Mamoraro Maimela <4323138@myuwc.ac.za>
2026-05-31 18:47:20 +0200

    Restarted the whole page again and the page is beign written in one file named progress.jsx ,and currently I'm done with nav bar.

--- uwc-cam-champion-site/src/screens/Progress.jsx
+++ uwc-cam-champion-site/src/screens/Progress.jsx
@@ -1,5 +1,5 @@
-import ProjectionCard from "../components/Progress/ProjectionCard.jsx";
 
+import "../components/Progress.css";
 
 
 
@@ -9,8 +9,31 @@ function Progress() {
 
     return (
         <>
-       
-        <ProjectionCard/>
+
+      <div  className="min-h-screen w-full">
+
+        <div className="top-nav">
+
+            <div className="left">
+              <div className="heading">Semester one</div>
+            </div>
+
+            <div>
+
+                <div className="drop-down-menu" >
+                    <button className="drop-down-btn">Modules</button>
+                      <div className="drop-down-content">
+                         <a href="#">module1</a>
+                         <a href="#">module2</a>
+                         <a href="#">module3</a>
+                      </div>
+             
+                </div>
+            </div>
+
+       </div>
+
+      </div>
         </>   
     );
 }
