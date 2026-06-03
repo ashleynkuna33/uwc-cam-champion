d08772764a57cd8786e444678b3da547f6a71648
Tshepho Mamoraro Maimela <4323138@myuwc.ac.za>
2026-06-04 00:31:19 +0200

    added a slider bar, and headings

--- uwc-cam-champion-site/src/screens/Progress.jsx
+++ uwc-cam-champion-site/src/screens/Progress.jsx
@@ -2,9 +2,23 @@
 import "../components/Progress.css";
 
 
-
+import { useRef, useState } from "react";
 
 function Progress() {
+
+
+  const barRef= useRef(null);
+  const [cam, setcam] = useState(65);
+
+  const camMark =(event)=>{
+
+    const bar =barRef.current.getBoundingClientRect();
+    const clickX = e.clientX - bar.left;
+    const percent = Math.min(100, Math.max(0, (clickX / bar.width) * 100));
+    setCam(Math.round(percent));
+   
+
+  }
 
 
     return (
@@ -109,11 +123,40 @@ function Progress() {
 
             <div className="left-card">
 
+                <div className="left-card-header">
+                    <p className="left-card-header-text"><span>PROJECTION SLIDER</span></p>
+                    <span className="left-card-activities">pending 2</span>
+                </div>
+
+                <div className="left-card-progress-bar">
+                    <div className="process-bar-header">
+
+                        <div className="bar-header">
+                            <h5>ASSIGNMENT</h5>
+                            <p>10% WEIGHT</p>
+                        </div>
+
+                        <div className="left-bar-line" ref={barRef} onClick={camMark}>
+                           <span style={{width:`${cam}%`}}></span>
+                            <div className="left-line-thumb" style={{left: `${cam}%`}}></div>
+                        </div>
+
+                        
+                    </div>
+
+                    <div className="under-line-measurement">
+                        <span style={{left:'0%'}}>0%</span>
+                        <span style={{left:'50%'}}>50%</span>
+                        <span style={{left:'75%'}}>75%</span>
+                        <span style={{left:'100%'}}>100%</span>
+                    </div>
+                </div>
+
             </div>
 
 
 
-            
+
 
             <div className="right-card">
 
