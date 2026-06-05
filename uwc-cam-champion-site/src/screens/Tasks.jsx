c4a7eae7f276bcde3c0e9c368e858ffda0f0b471
Ashley Nkuna <ashleynkuna33@gmail.com>
2026-06-05 14:13:56 +0200

    bb

--- uwc-cam-champion-site/src/screens/Tasks.jsx
+++ uwc-cam-champion-site/src/screens/Tasks.jsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 // icons
 import { LuFileText, LuPlus, LuPencil, LuFolder, LuCircleHelp, LuSettings } from "react-icons/lu";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
 
 const searchBar = () => {}
 
-const userHeader = () => {}
-
-const MenuIcon = ({ name: Icon, size, title, onPress }) => {
+const MenuIcon = ({ name: Icon, size, title, onPress, disableHover = false }) => {
     return (
-        <div onClick={onPress} className="cursor-pointer hover:text-white/70 transition-colors">
+        <div
+            onClick={onPress}
+            className={`cursor-pointer transition-colors ${!disableHover && "hover:text-white/70"}`}
+        >
             <Icon size={size} title={title}/>
+        </div>
+    );
+}
+
+const UserHeader = ({ name = "Unknown", surname = "Unknown", isLogged = false }) => {
+    return (
+        <div className="flex flex-row gap-2 items-center justify-center">
+            <MenuIcon name={FaRegUserCircle} size={22} disableHover />
+            {isLogged ? (
+                <div className="flex flex-row items-center gap-2">
+                    <div className="flex flex-col">
+                        <h1 className="text-sm font-bold">{name}</h1>
+                        <h1 className="text-sm font-bold">{surname}</h1>
+                    </div>
+                    <MenuIcon name={IoIosArrowDown} size={22} />
+                </div>
+            ) : (
+                <h1 className="text-sm font-bold">Log In</h1>
+            )}
         </div>
     );
 }
@@ -20,6 +42,7 @@ function Tasks() {
 
     return (
         <div className="min-h-screen w-full bg-black/10 flex flex-col">
+            
             <div className="bg-blue-500 py-6 flex items-center justify-between">
                 <h1 className="font-bold text-3xl pl-2">Modules</h1>
                 {/* will hold the search bar and user header */}
@@ -27,9 +50,7 @@ function Tasks() {
                     <div>
                         <h1>search bar</h1>
                     </div>
-                    <div>
-                        <h1>User header</h1>
-                    </div>
+                    <UserHeader />
                 </div>
             </div>
             <div className="grid grid-cols-[auto_1fr] flex-1">
