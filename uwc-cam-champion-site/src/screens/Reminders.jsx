e4c710338631b8f8d1f8ccedceac80492f698c96
Ashley Nkuna <ashleynkuna33@gmail.com>
2026-06-10 13:14:01 +0200

    aa

--- uwc-cam-champion-site/src/screens/Reminders.jsx
+++ uwc-cam-champion-site/src/screens/Reminders.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
+
+
 function Reminders() {
     return (
         <div>
             <div>
                 <h1>Reminders</h1>
             </div>
+
         </div>
     )
 }
