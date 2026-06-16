ec44830db89959f530e8a961184f0ed74f29e2cd
Ashley Nkuna <ashleynkuna33@gmail.com>
2026-06-16 16:40:50 +0200

    ll

--- uwc-cam-champion-site/src/screens/Settings.jsx
+++ uwc-cam-champion-site/src/screens/Settings.jsx
@@ -1,4 +1,6 @@
-
+import { FaKey } from "react-icons/fa";
+import { IoMdAdd } from "react-icons/io";
+import { SiMonkeytie } from "react-icons/si";
 
 export default function Settings () {
     return (
@@ -13,11 +15,11 @@ export default function Settings () {
                 {/* buttons */}
                 <div className="flex flex-row gap-4 items-center">
                     {/* cancel button */}
-                    <div className="border border-gray-500 p-1 rounded-md">
+                    <div className="border border-gray-500 p-1 rounded-md transition-all duration-300 hover:bg-gray-500 cursor-pointer">
                         <h1 className="font-bold text-sm">Cancel</h1>
                     </div>
                     {/* save button */}
-                    <div className="border p-1 rounded-md bg-black">
+                    <div className="border p-1 rounded-md bg-black cursor-pointer">
                         <h1 className="font-bold text-sm text-white">Save</h1>
                     </div>
                 </div>
@@ -50,7 +52,7 @@ export default function Settings () {
                                 <h1 className="font-bold">Update Password</h1>
                                 <p className="font-bold text-sm text-gray-600">Change your password to update & protect your Account</p>
                             </div>
-                            <div className="flex border border-gray-500 rounded-md p-1 items-center justify-center">
+                            <div className="flex border border-gray-500 rounded-md p-1 items-center justify-center transition-all duration-300 hover:bg-gray-500 cursor-pointer">
                                 <h1 className="font-bold text-sm">Change Password</h1>
                             </div>
                         </div>
@@ -67,7 +69,7 @@ export default function Settings () {
                                 <p className="font-bold text-sm text-gray-500">Setup Recovery Email to Secure your Account</p>
                             </div>
                             <div className="border p-1 rounded-md bg-black">
-                                <h1 className="font-bold text-sm text-white">Save</h1>
+                                <h1 className="font-bold text-sm text-white cursor-pointer">Save</h1>
                             </div>
                         </div>
                         {/* additional email space */}
@@ -86,7 +88,7 @@ export default function Settings () {
                                 <p className="font-bold text-gray-500 text-sm">Add Phone Number to Setup SMS Recovery for your account</p>
                             </div>
 
-                            <div className="flex items-center border w-16 p-1 rounded-md">
+                            <div className="flex items-center justify-center border w-16 p-1 rounded-md transition-all duration-300 hover:bg-gray-500 cursor-pointer">
                                 <h1 className="font-bold">Setup</h1>
                             </div>
                         </div>
@@ -97,7 +99,7 @@ export default function Settings () {
                                 <h1 className="font-bold">Deactivate Account</h1>
                                 <p className="font-bold text-sm text-gray-500">This will shut down your account. And it will reactivate with Signing in</p>
                             </div>
-                            <div className="border border-gray-500 bg-gray-500/30 rounded-md flex items-center justify-center p-1">
+                            <div className="border border-red-500/60 bg-gray-500/30 rounded-md flex items-center justify-center p-1 transition-all duration-300 hover:bg-red-500/50  cursor-pointer">
                                 <h1 className="font-bold text-gray-700">Deactivate</h1>
                             </div>
                         </div>
@@ -120,21 +122,27 @@ export default function Settings () {
                     <div className="h-px w-full bg-gray-300 my-4"></div>
                     <div className="flex flex-row items-center justify-between p-2">
                         <div className="flex flex-row">
-                            <div>Icon</div>
+                            <div className="p-2 flex bg-gray-500/40 rounded-md mr-2">
+                                <SiMonkeytie size={16}/>
+                            </div>
                             <h1 className="font-bold">Authentication App</h1>
                         </div>
-                        <div>Icon</div>
+                        <div className="border border-gray-500 p-1 rounded-xl transition-all duration-200 hover:bg-gray-500 cursor-pointer">
+                            <IoMdAdd size={30}/>
+                        </div>
                     </div>
                     <div className="h-px w-full bg-gray-300 my-4"></div>
 
                     <div>
                         <div className="flex flex-row items-center gap-4">
-                            <div>Icon</div>
+                            <div className="p-2 bg-gray-500/40 rounded-md">
+                                <FaKey size={16}/>
+                            </div>
                             <h1 className="font-bold">Security Key</h1>
                         </div>
                         <p className="font-bold text-sm text-gray-500 my-1">Use physical security key to protect your account</p>
                         <div className="flex justify-center p-1 pb-2">
-                            <div className="border border-gray-500 rounded-md flex items-center justify-center w-[80%] p-1">
+                            <div className="border border-gray-500 rounded-md flex items-center justify-center w-[80%] p-1 transition-all duration-300 hover:bg-gray-500 cursor-pointer">
                             <h1 className="font-bold ">Use Security Key</h1>
                         </div>
                         </div>
