f46b4b2709aa26ce31a112762a14346b1837213f
ashley <ashleynkuna33@gmail.com>
2026-08-18 13:44:57 +0200

    2

--- uwc-cam-champion-site/src/context/UserContext.jsx
+++ uwc-cam-champion-site/src/context/UserContext.jsx
@@ -49,7 +49,7 @@ export const UserProvider = ({ children }) => {
 //   data
 
   return (
-    <UserContext.Provider value={{ user, isLoggedIn, loading, login, signUp, forgotPassword, handleContinueWithoutLogin }}>
+    <UserContext.Provider value={{ user, isLoggedIn, loading, login, signUp, forgotPassword, handleContinueWithoutLogin, modules, deadlines, cam, tasks }}>
       {children}
     </UserContext.Provider>
   );
