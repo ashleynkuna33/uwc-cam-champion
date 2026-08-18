87f3293d8b5a301750135b6fa35c42d90f7195ae
xolelamarasi27 <4346572@myuwc.ac.za>
2026-08-19 01:07:01 +0200

    Update module details and filter

--- uwc-cam-champion-site/src/context/UserContext.jsx
+++ uwc-cam-champion-site/src/context/UserContext.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const UserContext = createContext();
 
@@ -18,24 +17,24 @@ export const UserProvider = ({ children }) => {
 
 
 //   data
-  const [modules, setModules] = useState(null);
-  const [deadlines, setDeadlines] = useState(null);
+  const [modules, setModules] = useState([]);
+  const [deadlines, setDeadlines] = useState([]);
   const [cam, setCam] = useState(0);
-  const [tasks, setTasks] = useState(null);
+  const [tasks, setTasks] = useState([]);
 
 //   user authentication and session management logic here
 
   const login = async ({username, password}) => {
     try {} catch (error) {} finally {}
-  }
+  };
 
   const signUp = async ({firstname, surname, email, password}) => {
     try {} catch (error) {} finally {}
-  }
+  };
 
   const forgotPassword = async ({email}) => {
     try {} catch (error) {} finally {}
-  }
+  };
 
   const handleContinueWithoutLogin = () => {
     setUser({
@@ -44,12 +43,12 @@ export const UserProvider = ({ children }) => {
     });
   };
   
-  let isLoggedIn = Boolean(user)
+  const isLoggedIn = Boolean(user)
 
 //   data
 
   return (
-    <UserContext.Provider value={{ user, isLoggedIn, loading, login, signUp, forgotPassword, handleContinueWithoutLogin, modules, deadlines, cam, tasks }}>
+    <UserContext.Provider value={{ user, isLoggedIn, loading, login, signUp, forgotPassword, handleContinueWithoutLogin, modules, setModules,deadlines,  cam, tasks }}>
       {children}
     </UserContext.Provider>
   );
